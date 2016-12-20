cabd10303882fb20c92dbeb92f1b8b79656be17d
infullstack <infullstack@163.com>
2016-12-20 18:42:43 +0800

    init properties

---
+++ mdfs-datanode/src/main/java/com/infullstack/mdfs/datanode/file/FileReceiver.java
@@ -0,0 +1,17 @@
+package com.infullstack.mdfs.datanode.file;
+
+import com.infullstack.mdfs.common.utils.PropertiesUtils;
+
+/**
+ * Created by Ray on 2016/12/20 0020.
+ */
+public class FileReceiver {
+
+    public static void main(String args[]) {
+        PropertiesUtils.load("mdfs-site.properties");
+    }
+
+
+
+
+}
